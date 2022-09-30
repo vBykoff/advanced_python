887e1cdf5ab7ddcafca7f84eac84b5841c359cb6
vladimir <v.bykov.2002@gmail.com>
2022-09-30 16:13:47 +0300

    HW2

--- 02/json_parser.py
+++ 02/json_parser.py
@@ -3,23 +3,26 @@ import json
 
 
 def keyword_handler(keyword):
-    """Default print function for parse_json"""
-    print(keyword)
+    """Default function for parse_json"""
+    return keyword
 
 
 def parse_json(json_str: str, required_fields=None,
                keywords=None, keyword_callback=keyword_handler):
     """Find keywords in required fields in json string
     and execute keyword_callback for each of them"""
+    result = []
     json_doc = json.loads(json_str)
-    print(json_doc)
     for field in json_doc:
         if field in required_fields:
             for key in json_doc[field].split():
                 if key in keywords:
-                    keyword_callback(key)
+                    key = keyword_callback(key)
+                    result.append(key)
+    return result
 
 
 if __name__ == "__main__":
+
     parse_json('{"key1": "Word1 word2", "key2": "word2 word3"}',
                required_fields=["key1"], keywords=["word2"])
--- 02/tests.py
+++ 02/tests.py
@@ -1,9 +1,49 @@
-import unittest
+from faker import Faker
+from json_parser import parse_json, keyword_handler
+import json
+import random
+import unittest.mock
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class JsonParserTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        faker = Faker()
+        json_dict = {}
+        self.required_fields = []
+        self.keywords = []
+        self.result = []
+
+        for i in range(10):
+            random_key = faker.word()
+            random_value = faker.name()
+            json_dict[random_key] = random_value
+
+            if random.randint(0, 1):
+                self.required_fields.append(random_key)
+                word = random_value.split()[random.randint(0, 1)]
+
+                self.keywords.append(word)
+                self.result.append(word)
+
+        self.json_object = json.dumps(json_dict)
+        print(self.json_object)
+        print(self.result)
+        print(self.keywords)
+        print(self.required_fields)
+
+    def test_json_parser(self):
+        result = parse_json(str(self.json_object), self.required_fields, self.keywords)
+        self.assertEqual(result, self.result)
+
+    @unittest.mock.patch("json_parser.keyword_handler")
+    def test_json_parser_callback(self, keyword_handler_mock):
+        keyword_handler_mock.return_value = "Value"
+        result = parse_json(str(self.json_object), self.required_fields,
+                            self.keywords, keyword_handler_mock)
+        print(result)
+        self.assertTrue(keyword_handler_mock.called)
+        self.assertEqual(keyword_handler_mock.call_count, len(self.result))
 
 
 if __name__ == '__main__':
